51a05679be55a89282c23a77308394c987762e8b
james robert williams & alex caste <student@epicodus.com>
2015-04-21 10:47:04 -0700

    updated spec file

--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('weekday_finder')
 
 describe('String#weekday_finder') do
-# The program takes the weekday number and provides the name of the day of the week to the user.
+# The program takes the date the user the puts in and provides the day of the week of that day. 
  it("Takes the weekday number and provides the name of the day") do
    expect(("3/12/2014").weekday_finder()).to(eq("Wednesday"))
  end
