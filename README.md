c2f948436dcf201fea560e37c01e578354b905fd
james robert williams & alex caste <student@epicodus.com>
2015-04-21 11:02:53 -0700

    Updated Readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+Weekday_Finder application
+
+by James and Alex
+
+The weekday finder application allows a user to input any date in the format of "month/day/year".weekday_finder() and it will return the day of the week of that day.
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,8 +2,8 @@ require('rspec')
 require('weekday_finder')
 
 describe('String#weekday_finder') do
-# The program takes the date the user the puts in and provides the day of the week of that day. 
- it("Takes the weekday number and provides the name of the day") do
+# The program takes the date the user the puts in and provides the day of the week of that day.
+ it("Takes the date the user inputs and provides the name of the day") do
    expect(("3/12/2014").weekday_finder()).to(eq("Wednesday"))
  end
 end
