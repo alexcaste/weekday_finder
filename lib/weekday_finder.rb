0ef1530c5876d39be81c55fe7d822c1a8d16913f
james robert williams & alex caste <student@epicodus.com>
2015-04-21 10:46:16 -0700

    removed first spec, code passed,

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -8,5 +8,21 @@ class String
   day = the_date.at(1)
   current_date = Time.new(year, month, day)
   current_date.wday()
+
+    if current_date.wday().eql?(0)
+      "Sunday"
+    elsif current_date.wday().eql?(1)
+      "Monday"
+    elsif current_date.wday().eql?(2)
+      "Tuesday"
+    elsif current_date.wday().eql?(3)
+      "Wednesday"
+    elsif current_date.wday().eql?(4)
+      "Thursday"
+    elsif current_date.wday().eql?(5)
+      "Friday"
+    else current_date.wday().eql?(6)
+      "Saturday"
+    end
   end
-  end
+end
--- spec/weekday_finder_spec.rb
+++ spec/weekday_finder_spec.rb
@@ -2,12 +2,8 @@ require('rspec')
 require('weekday_finder')
 
 describe('String#weekday_finder') do
-# The program takes the date provided by the user and returns weekday number.
- it('Takes the date provided by user and provides number for weekday') do
-   expect(("3/11/2014").weekday_finder()).to(eq(2))
- end
 # The program takes the weekday number and provides the name of the day of the week to the user.
  it("Takes the weekday number and provides the name of the day") do
-   expect(("3/11/2014").weekday_finder()).to(eq("tuesday"))
+   expect(("3/12/2014").weekday_finder()).to(eq("Wednesday"))
  end
 end
